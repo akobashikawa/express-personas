952fbf576d2ff9e021857160204b32591257b740
Antonio Kobashikawa <akobashikawa@gmail.com>
2023-02-16 18:04:03 -0500

    Eliminar implementado

--- routes/personasRouter.js
+++ routes/personasRouter.js
@@ -3,10 +3,21 @@ const router = express.Router();
 
 const personasService = require('./personasService').create();
 
+router.get('/personas/', (req, res) => {
+  const personas = personasService.listar();
+  return res.json(personas);
+});
+
 router.post('/personas/', (req, res) => {
   const nombre = req.body.nombre;
   const nuevaPersona = personasService.agregar(nombre);
   return res.json(nuevaPersona);
+});
+
+router.delete('/personas/:id', (req, res) => {
+  const id = req.params.id;
+  const personaEliminada = personasService.eliminar(id);
+  return res.json(personaEliminada);
 });
 
 module.exports = router;
--- routes/personasService.js
+++ routes/personasService.js
@@ -1,17 +1,36 @@
 const personasService = {
+    id: null,
     personas: null,
 
     create() {
         const obj = Object.create(this);
+        obj.id = 0;
         obj.personas = [];
         return obj;
     },
 
+    listar() {
+        // console.log(this.personas);
+        return this.personas;
+    },
+
     agregar(nombre) {
-        const persona = { nombre };
+        this.id = this.id + 1;
+        const persona = { id: this.id, nombre };
+        // console.log({persona});
         this.personas.push(persona);
         return persona;
-    }
+    },
+
+    eliminar(id) {
+        const foundIndex = this.personas.findIndex(item => item.id == id);
+        // console.log({foundIndex});
+        const personaEliminada = this.personas.splice(foundIndex, 1);
+        // console.log({personaEliminada});
+        // console.log(this.personas);
+        return personaEliminada;
+    },
+    
 
 
 };
