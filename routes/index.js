02b652ed047f4d9cb956d97fc7777f1418f45a00
Antonio Kobashikawa <akobashikawa@gmail.com>
2023-02-25 20:55:11 -0500

    Corregido caso holamundo

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+router.get('/hola/', (req, res) => {
+  return res.json({
+    saludo: `Hola Mundo!`
+  });
+});
+
 router.post('/hola/', (req, res) => {
   const nombre = req.body.nombre;
   return res.json({
