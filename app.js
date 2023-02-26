55a2d19aaad49ad68a213a4736b99a8c0fd0a4e1
Antonio Kobashikawa <akobashikawa@gmail.com>
2023-02-25 21:05:30 -0500

    Agregado cors

--- app.js
+++ app.js
@@ -2,11 +2,14 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors');
 
 var holaRouter = require('./routes/holaRouter');
 var personasRouter = require('./routes/personasRouter');
 
 var app = express();
+
+app.use(cors());
 
 app.use(logger('dev'));
 app.use(express.json());
